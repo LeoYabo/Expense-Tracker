7af40c3b6833dece5139a1cc9ff7a0b425b7cb33
Leonel Yabo <leonel.yabo@devalore.com>
2022-06-29 10:37:42 +0300

    total maximum of of chart is sum of all expenses

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -7,10 +7,11 @@ const Chart = (props) => {
 
     //creating an array consisting of the values of the values inside the dataPoint array that is full of objects
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value)
+    console.log(dataPointValues)
     
     //math.max(<values spread by commas>) finds the biggest value in an array and returns it
     //because the max() method doesnt take an array, we can use the ... operator to pull the values out of the array, to be spread
-    const totalMaximum = Math.max(...dataPointValues)
+    const totalMaximum = dataPointValues.reduce((a, b) => a + b, 0)
 
     return(
         <div className='chart'>
